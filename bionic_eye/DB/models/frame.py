e9ab6d6c143a2a44f1f27626ec69f6eee08fe43f
yotam201410 <yotam201410@gmail.com>
2024-06-10 09:39:39 +0300

    renamed frame value

--- bionic_eye/DB/models/frame.py
+++ bionic_eye/DB/models/frame.py
@@ -8,7 +8,7 @@ class Frame(Base):
     __tablename__ = 'frames'
     id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     video_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey('videos.id'))
-    object_storage_path: Mapped[str] = mapped_column(String, nullable=False)
+    storage_path: Mapped[str] = mapped_column(String, nullable=False)
     frame_index: Mapped[int] = mapped_column(Integer, nullable=False)
     metadata_id: Mapped[uuid.UUID] = mapped_column(UUID(as_uuid=True), ForeignKey('metadata.id'))
 
