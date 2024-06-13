30db298769ec6dd3a0913fa14501e8a4022f6f03
yotam201410 <yotam201410@gmail.com>
2024-06-13 13:48:29 +0300

    added getting path and paths handlers

--- bionic_eye/backend/controllers/video_controller.py
+++ bionic_eye/backend/controllers/video_controller.py
@@ -1,4 +1,6 @@
 import uuid
+from pathlib import Path
+from typing import List
 
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -16,3 +18,17 @@ async def create_video_handler(video: VideoInput, session: AsyncSession = Depend
     video = await video_service.addVideo(video.storage_path)
     await session.flush()
     return video.id
+
+
+@video_router.get("/{id}/path", status_code=200)
+async def get_video_path_handler(id: uuid.UUID, session: AsyncSession = Depends(get_db)) -> Path:
+    video_service = VideoService(session)
+    path = await video_service.getVideoPath(id)
+    return path
+
+
+@video_router.get("/path", status_code=200)
+async def get_videos_paths_handler(session: AsyncSession = Depends(get_db)) -> List[Path]:
+    video_service = VideoService(session)
+    paths = await video_service.getVideosPath()
+    return paths
