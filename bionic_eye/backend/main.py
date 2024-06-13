1a678b709b20a0e8506d26632955683d9ade6fd9
yotam201410 <yotam201410@gmail.com>
2024-06-13 18:01:39 +0300

    updated title and description

--- bionic_eye/backend/main.py
+++ bionic_eye/backend/main.py
@@ -2,10 +2,10 @@ import uvicorn
 from fastapi import FastAPI
 
 from bionic_eye.backend.controllers.video_controller import video_router
-
+from bionic_eye.backend.middelwares.error_middleware import ErrorLoggingMiddleware
 app = FastAPI(
-    title="Noted API", description="This is a simple note taking service", docs_url="/")
-app.router.include_router(video_router)
+    title="Bionic Eye API", docs_url="/docs")
 
+app.router.include_router(video_router)
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8080)
