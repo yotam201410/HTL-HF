c20f8f2620bd74422ec85b77bdca24f710ec882c
yotam201410 <yotam201410@gmail.com>
2024-06-16 11:15:29 +0300

    removed import and added router in BP

--- bionic_eye/backend/main.py
+++ bionic_eye/backend/main.py
@@ -2,10 +2,10 @@ import uvicorn
 from fastapi import FastAPI
 
 from bionic_eye.backend.controllers.video_controller import video_router
-from bionic_eye.backend.middelwares.error_middleware import ErrorLoggingMiddleware
+
 app = FastAPI(
     title="Bionic Eye API", docs_url="/docs")
 
-app.router.include_router(video_router)
+app.include_router(video_router)
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8080)
